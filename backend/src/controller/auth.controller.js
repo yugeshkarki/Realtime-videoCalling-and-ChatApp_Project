7eb143cbac728557127c7a283b3b6d8b556ba2c3
yugesh karki <yugeshkarki1@gmail.com>
2026-05-29 11:51:20 +0545

    updated backend code

---
+++ backend/src/controller/auth.controller.js
@@ -0,0 +1,137 @@
+ import { upsertStreamUser } from '../lib/stream.js';
+import User from '../models/User.js';
+import jwt from 'jsonwebtoken';
+export  async function  signup(req, res){
+  const {email ,password,fullName}=req.body;
+  try {
+   if(!fullName|| !password || !email){
+     return res.status(400).json({message:"All fieleds are required"});
+   }
+   if(password.length < 6){
+      return res.status(400).json( { success:false,message:"password must be at least 6 character"});
+   }
+
+   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+ if (!emailRegex.test(email)) {
+  return res.status(400).json({ message: "Invalid email format" });
+}
+ const existingUser=await User.findOne({email});
+ if(existingUser){
+   return res.status(400).json({message:"Email already exists,please use a diffrent email one"})
+ }
+
+   const idx=Math.floor(Math.random()* 100)+1;
+   const randomAvatar=`https://avatar.iran.liara.run/public/${idx}.png`
+
+   const newUser=await User.create({
+      email,fullName,password,
+      profilePic:randomAvatar
+   })
+  
+try {
+     await upsertStreamUser({
+    id:newUser._id.toString(),
+   name:newUser.fullName,
+   image:newUser.profilePic|| "",
+  })
+  console.log(`Stream user created for ${newUser.fullName}`)
+   
+} catch (error) {
+   console.log("Error creating Stream user:",error)
+}
+   
+  const token=jwt.sign({userId:newUser._id},process.env.JWT_SECRET_KEY,{
+      expiresIn:"7d",
+   })
+   res.cookie('jwt',token,{
+      maxAge:7 * 24 * 60 * 60 * 1000,
+      httpOnly:true,        //prevents XSS Attacks
+      sameSite:"Strict",    //prevents CSRF Attack
+      secure:process.env.NODE_ENV === "production"
+   })
+   res.status(201).json({success:true, user:newUser})
+
+  } catch (error) {
+   console.log("error in signup controller",error);
+   res.status(500).json({success:false,message:error.message});
+  }
+ }
+
+ export async function login(req,res){
+  try {
+   const {email ,password}=req.body;
+   if(!email ||! password){
+      return res.status(400).json({message:"All fields are required"});
+   }
+const user=await User.findOne({email})
+   if(!user){
+      return res.status(401).json({message:"Invaild email or password"})
+   }
+  const isPasswordCorrect=await user.matchPassword(password);
+  if(!isPasswordCorrect) return res.status(401).json({message:"Invalid email or password"});
+   
+    const token=jwt.sign({userId:user._id},process.env.JWT_SECRET_KEY,{
+      expiresIn:"7d",
+   })
+   res.cookie('jwt',token,{
+      maxAge:7 * 24 * 60 * 60 * 1000,
+      httpOnly:true,        //prevents XSS Attacks
+      sameSite:"Strict",    //prevents CSRF Attack
+      secure:process.env.NODE_ENV === "production"
+   })
+   res.status(200).json({success:true, user})
+
+  } catch (error) {
+   console.log("error in login Controller", error.message);
+   res.status(500).json({success:false,message:error.message});
+  }
+ }
+ ``
+  export async function logout(req,res){
+    res.clearCookie("jwt"),
+    res.status(200).json({success:true,message:"Logout Successfull"});
+ }
+
+ export async function onboard(req,res){
+ 
+   try {
+      const userId=req.user._id;
+      const {fullName,nativeLanguage ,bio, learningLanguage, location}=req.body;
+
+      if(!fullName ||! bio ||!nativeLanguage || !learningLanguage ||!location){
+         return res.status(400).json({
+            message:"All fields are required",
+            missingFields:[
+               !fullName && "fullName",
+               !bio && "bio",
+               !nativeLanguage && "nativeLanguage",
+               !learningLanguage && "learningLanguage",
+               !location && "location"
+            ].filter(Boolean),
+         })
+      }
+      const updatedUser=await User.findByIdAndUpdate(userId,{
+         ...req.body,
+         isOnboarded:true,
+      },{new:true})
+
+      if (!updatedUser)    return res.status(404).json({message:"Unauthorized user not found"});
+        try {
+         await upsertStreamUser({
+            id:updatedUser._id.toString(),
+            name:updatedUser.fullName,
+            image:updatedUser.profilePic|| ""
+         });
+         console.log(`stream user updated after onboarding for ${updatedUser.fullName}`);
+         
+        } catch (streamError) {
+         console.log("Error updating stream user during onboarding:",streamError.message);
+        }
+
+       res.status(200).json({success:true, user:updatedUser});
+   } catch (error) {
+      console.error("onboarding error",error);
+      res.status(500).json({message:"internal server error"});
+   }
+ }
